ce7694cac37ea04f6882ec20f86ca605990230db
Daeeui Kim <kimeodml@gmail.com>
2024-09-23 22:17:26 +0900

    day10

--- app-router/src/app/(with-searchbar)/layout.tsx
+++ app-router/src/app/(with-searchbar)/layout.tsx
@@ -1,7 +1,9 @@
+import SearchBar from '@/components/Searchbar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div>Searchbar Layout</div>
+      <SearchBar />
       {children}
     </div>
   );
---
+++ app-router/src/components/Searchbar.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function SearchBar() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [search, setSearch] = useState('');
+  const q = searchParams.get('q') || '';
+
+  useEffect(() => {
+    setSearch(q);
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || search === q) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == 'Enter') {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+        placeholder={'검색어를 입력하세요...'}
+      />
+      <button onClick={onSubmit}>검색</button>
+    </div>
+  );
+}
